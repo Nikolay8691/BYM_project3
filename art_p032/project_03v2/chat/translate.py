6cf42d4f7042e306358e6313fa1ae54bd656e3af
LAPTOP-FMOIC7M3\nv-ma <nv-mail@mail.ru>
2023-02-22 09:23:42 +0200

    callback_query Response is ready for database

--- art_p032/project_03v2/chat/translate.py
+++ art_p032/project_03v2/chat/translate.py
@@ -1,12 +1,129 @@
 import random
 
-def get_translation(sentence):
+import json
 
-	se = ''
+from .models import TelegramMessage
+
+languages_choose_from = {
+						'es' : 'espana', 
+						'fr' : 'ranska', 
+						'it' : 'italia', 
+						'de' : 'saksa', 
+						'fi' : 'suomi', 
+						'ru' : 'venäjä', 
+						'ko' : 'korea'
+					}
+
+# def get_translation(sentence) -> tuple:
+
+# 	se = ''
+# 	if sentence[0] == 'suomi':
+# 		se = input(f' translate : {sentence[1]} - ')
+# 		reply_status == True
+# 		reply = f'sinä kirjoitit - {sentence[1]}. suomeksi se on {se}'
+
+# 	else:
+# 		times = random.randint(1, 10)
+# 		reply_status = not(times == 1)
+# 		reply = f'sinä kirjoitit - {sentence[1]}. {sentence[0]}ksi se on ... en tiedä vielä.\n just try {times} times more and you will know!'
+
+# 	return (reply, reply_status)
+
+def get_translation(sentence) -> dict:
+
+	reply = {}
+	# se = ''
 	if sentence[0] == 'suomi':
 		se = input(f' translate : {sentence[1]} - ')
-		return f'sinä kirjoitit - {sentence[1]}. suomeksi se on {se}'
+		reply['status'] = True
+		reply['text'] = f'sinä kirjoitit - {sentence[1]}. suomeksi se on {se}'
 
 	else:
+		se = 'no_translation'
 		times = random.randint(1, 10)
-		return f'sinä kirjoitit - {sentence[1]}. {sentence[0]}ksi se on ... en tiedä vielä.\n just try {times} times more and you will know!'
+		reply['status'] = not(times == 1)
+		reply['text'] = f'sinä kirjoitit - {sentence[1]}. {sentence[0]}ksi se on ... en tiedä vielä.\n just try {times} times more and you will know!'
+
+	reply['word_original'] = sentence[1]
+	reply['word_translation'] = se
+
+	return reply
+
+
+def process_callbackquery(message):
+
+	message_body = message["callback_query"]["message"]
+	chat_id = message_body["chat"]["id"]
+	word = message_body["text"]
+
+	word_and_translation = message_body["reply_markup"]["inline_keyboard"][0][0]["callback_data"]
+	
+	words = word_and_translation.split(':')
+	word_original = words[0]
+	word_translation = words[1]
+
+	reply = f"Okay, {word_original} with {word_translation} is in FDB now"
+	
+	return {"chat_id": chat_id, "text": reply}
+
+
+def process_command(message):
+
+	text = message["message"]["text"]
+	chat_id = message["message"]["chat"]["id"]
+
+	if text == "/newwords":
+		languages = ",\n".join(f'   {code} : {language}' for code, language in sorted(languages_choose_from.items()))
+		languages_list_title = "languages to choose from "
+		invitation = "\ntype the word you want to know, like\nlanguage_code: new_word (ex. de: hello)"
+		reply = f"{languages_list_title} :\n{languages}\nwelcome to '{text.lstrip('/')}' study : {invitation}"
+
+	elif text == "/newtest":
+		reply = f"welcome to {text.lstrip('/')} exercise :\n its' logic is not ready yet. "
+
+	else:
+		reply = f"Smth. is wrong, check your inputs!"
+
+	return {"chat_id": chat_id, "text": reply}
+
+
+def process_text_message(message):
+
+	name = message["message"]["from"]["first_name"]
+	text = message["message"]["text"]
+	chat_id = message["message"]["chat"]["id"]
+
+	words = text.split(' ')
+	language = words[0].lower().rstrip(':')
+
+	if len(words) > 1 and (language in languages_choose_from):
+
+		to_translate = ' '.join(words[1:])
+		sentence = (languages_choose_from[language], to_translate)
+		# reply, reply_status = get_translation(sentence)
+		reply_dict = get_translation(sentence)
+		# reply_status = reply_dict['status']
+		# reply_text = reply_dict['text']
+		# reply = json.dumps(reply_dict)
+
+		# if reply_status:
+		if reply_dict['status']:
+			TelegramMessage.objects.create(name = name, message = text, chat_id = chat_id)
+
+			callback_data = f"{reply_dict['word_original']}:{reply_dict['word_translation']}"
+			keyboard_array = [[{'text' : ' += FDB (store in _my favorites_ database)', 'callback_data' : callback_data}]]
+			reply_markup_json = json.dumps({'inline_keyboard' : keyboard_array}, separators=(',', ':'))
+
+			# return {"chat_id": chat_id, "text": reply_text, "reply_markup" : reply_markup_json}
+			return {"chat_id": chat_id, "text": reply_dict["text"], "reply_markup" : reply_markup_json}
+
+		else:
+
+			reply = 'Strange - nobody knows this word but you. \nSend it to Oxford : oxford-new@gmail.com\n they will check it out!'
+			return {"chat_id": chat_id, "text": reply}
+
+	else:
+		reply = 'Invalid command - check the message and/or language code. Send like - de: girl - to translate girl to german'
+		return {"chat_id": chat_id, "text": reply}
+
+	# reply = f"hei {name}! Oon saannut viestasi {text}"
--- art_p032/project_03v2/chat/utils.py
+++ art_p032/project_03v2/chat/utils.py
@@ -6,89 +6,29 @@ from django.conf import settings
 
 from .models import TelegramMessage
 
-from .translate import get_translation
+# from .translate import get_translation
+from .translate import process_callbackquery, process_command, process_text_message
 
 # from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def process_telegram_message(message):
 
 	pprint(message)
-	data_s = ''
 
 	if 'callback_query' in message:
+		data = process_callbackquery(message)
 
-		reply = f"welcome to callback_query :\n type smth."
-		chat_id = message["callback_query"]["message"]["chat"]["id"]
+	elif '/' in message['message']['text']:
+		data = process_command(message)
 
-	else :
+	else:
+		data = process_text_message(message)
 
-		name = message["message"]["from"]["first_name"]
-		text = message["message"]["text"]
-		chat_id = message["message"]["chat"]["id"]
-
-		if '/' in text:
-
-			reply = f"welcome to {text.lstrip('/')} :\n type the word you want to know"
-
-		else:
-
-			TelegramMessage.objects.create(name = name, message = text, chat_id = chat_id)
-
-			words = text.split(' ')
-			language = words[0].lower().rstrip(':')
-			languages_choices = {
-									'es' : 'espana', 
-									'fr' : 'ranska', 
-									'it' : 'italia', 
-									'de' : 'saksa', 
-									'fi' : 'suomi', 
-									'ru' : 'venäjä', 
-									'ko' : 'korea'
-								}
-
-			if len(words) > 1 and (language in languages_choices):
-				to_translate = ' '.join(words[1:])
-				sentence = (languages_choices[language], to_translate)
-				reply = get_translation(sentence)
-
-			else:
-				reply = 'Invalid command. Send like - de girl - to translate girl to german'
-
-			# reply = f"hei {name}! Oon saannut viestasi {text}"
-
-			keyboard_array = [
-				[
-					{'text' : 'option_1', 'callback_data' : 'right'},
-					{'text' : 'option_2', 'callback_data' : 'left'}
-				],
-				[
-					{'text' : 'option_3', 'callback_data' : 'up'}
-				]
-			]
-
-			reply_markup_dict = {'inline_keyboard' : keyboard_array}
-			reply_markup_json = json.dumps({'inline_keyboard' : keyboard_array}, separators=(',', ':'))
-
-			# print('reply_markup.inline_keyboard / type : ', type(reply_markup))
-			# pprint(reply_markup.inline_keyboard)
-
-			data_s = {"chat_id": chat_id, "text": reply, "reply_markup" : reply_markup_json}
-
-			# print('\n data :')
-			# pprint(data)
-
-			# print('\n data_s :')
-			# pprint(data_s)
-
-	data = {"chat_id": chat_id, "text": reply}
+	# data = {"chat_id": chat_id, "text": reply}
 
 	reply_url = f"https://api.telegram.org/bot{settings.TELEGRAM_API_TOKEN}/sendMessage"
 
-	if data_s:
-		r = requests.post(reply_url, data = data_s)
-	else:
-		r = requests.post(reply_url, data = data)
-	
+	r = requests.post(reply_url, data = data)
 
 	print(' Response : (utils.py) ', type(r))
 	print(r.json())
